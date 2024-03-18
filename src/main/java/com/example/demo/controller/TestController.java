cf846399233472498d8d49f3aef7257160c51c89
jiyoung <gyjeong021@gmail.com>
2024-03-18 17:44:23 +0900

    DTO와 컨트롤러 레이어

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("test") // "/test"도 상관없음
+public class TestController {
+
+    @GetMapping
+    public String testController() {
+        return "Hello World!";
+    }
+
+    @GetMapping("/testGetMapping")
+    public String testControllerWithPath() {
+        return "Hello World! testGetMapping";
+    }
+
+    @GetMapping("/{id}") // {} 중괄호나오면 PathVariable
+    public String testControllerWithPathVariable(@PathVariable int id) {
+        return "Hello World! ID " + id;
+    }
+
+    @GetMapping("/testRequestParam")
+    public String testControllerRequestParam(@RequestParam(required = false) int id) {
+        return "Hello World! ID " + id;
+    }
+
+}
