5a64b895c5468e7108e402896ee85c13e8c27d5f
jiyoung <gyjeong021@gmail.com>
2024-03-25 17:15:07 +0900

    퍼시스턴스 레이어

--- src/main/java/com/example/demo/Service/TodoService.java
+++ src/main/java/com/example/demo/Service/TodoService.java
@@ -1,10 +1,22 @@
 package com.example.demo.Service;
 
+import com.example.demo.model.TodoEntity;
+import com.example.demo.repository.TodoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TodoService {
+    @Autowired
+    private TodoRepository repository;
+
     public String testService() {
-        return "Test Service";
+        TodoEntity entity = TodoEntity.builder().title("My first Todo item").build();
+
+        repository.save(entity);
+
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+
+        return savedEntity.getTitle();
     }
 }
