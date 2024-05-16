aec1b5861ec596e07811356c9611742a58cd9fb9
jiyoung <gyjeong021@gmail.com>
2024-05-16 23:04:16 +0900

    액서스 토큰 로컬 스토리지에 저장하여 해결

--- src/ApiService.js
+++ src/ApiService.js
@@ -1,11 +1,16 @@
 import { API_BASE_URL } from './api-config';
 
 export function call(api, method, request) {
-    let options = {
-        headers: new Headers({
-            "Content-Type": "application/json",
+    let headers = new Headers({
+        "Content-Type":"application/json",
+    });
 
-        }),
+    const accessToken = localStorage.getItem("ACCESS_TOKEN");
+    if (accessToken && accessToken != null) {
+        headers.append("Authorization", "Bearer " + accessToken);
+    }
+    let options = {
+        headers: headers,
         url: API_BASE_URL + api,
         method: method,
     };
@@ -34,6 +39,7 @@ export function signin(userDTO) {
     return call("/auth/signin", "POST", userDTO)
         .then((response) => {
             if(response.token) {
+                localStorage.setItem("ACCESS_TOKEN", response.token);
                 window.location.href = "/";
             }
         });
